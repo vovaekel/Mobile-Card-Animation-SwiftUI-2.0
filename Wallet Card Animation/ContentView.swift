9ea76b8f6e68497a6b5f2b212fbc3eb274db8336
Kalinichenko <vova.kalinichenko@my.com>
2021-05-03 21:29:22 +0300

    Initial Commit

---
+++ Wallet Card Animation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Wallet Card Animation
+//
+//  Created by Владимир Калиниченко on 03.05.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Wallet Card Animation/Wallet_Card_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wallet_Card_AnimationApp.swift
+//  Wallet Card Animation
+//
+//  Created by Владимир Калиниченко on 03.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct Wallet_Card_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
